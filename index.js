dda2ce64b36911cbc8514018b230f0d99ecb9cc9
WoozyStudio <laurapinales412@gmail.com>
2023-05-06 08:10:11 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const { Client, Message, Collection } = require('discord.js');
+const config = require('./config.json');
+const fs = require('fs');
+const client = new Client({
+    partials: ["MESSAGE", "CHANNEL", "REACTION"],
+    intents: 32767,
+});
+
+module.exports = client;
+
+const prefix = config.prefix;
+
+client.commands = new Collection();
+client.aliases = new Collection();
+client.events = new Collection();
+client.slashCommands = new Collection();
+
+client.categories = fs.readdirSync('./commands');
+
+//Load all the files
+['command', 'events', 'slashCommands'].forEach((handler) => {
+    require(`./handlers/${handler}`)(client)
+});
+
+client.login(config.token)
